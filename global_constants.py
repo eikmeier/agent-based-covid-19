8227595d2e658a8d2c6e0ced4735e726d6677bf0
Erik Nelson <erikdnel@gmail.com>
2021-06-14 00:31:42 -0500

    Add global constants to the program that may be needed in multiple different files

---
+++ global_constants.py
@@ -0,0 +1,18 @@
+PASSING_TIME = 10 # Common passing time between classes, in minutes(302-307)
+TOTAL_AGENTS = 2380 # 1500 on-campus students, 500 off-campus students, and 380 faculties (217-218)
+SPACE_CAPACITIES = {"Transit Space": 100 * TOTAL_AGENTS, "Dining Hall": 650, "Library": 300 * PASSING_TIME,
+                    "Gym": 60 * PASSING_TIME}
+SPACE_RISK_MULTIPLIERS = {"Transit Space": 1, "Dining Hall": 1, "Library": 1, "Gym": 3,
+                          "Office": 1, "Large Gatherings": 3, "Academic": 1, "Dorm": 2}
+SUBSPACE_CAPACITIES = {"Dining Hall": 100, "Faculty Dining Leaf": 20, "Library": 50, "Gym": 10,
+                       "Social Space": 10}
+SUBSPACE_RISK_MULTIPLIERS = {"Dining Hall": 2, "Faculty Dining Leaf": 2, "Library": 2, "Gym": 3,
+                             "Office": 2, "Social Space": 3, "Classroom": 2, "Dorm": 3}
+
+"""
+VARIABLE CV/RVs:
+    Space CV: 
+    Space RV:
+    Subspace CV:
+    Subspace RV:
+"""
