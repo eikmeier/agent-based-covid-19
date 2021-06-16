56bd08530dd0e69863185dc041d1c6b9ef92d454
lilgiant2001 <changjy01@gmail.com>
2021-06-16 23:53:37 +0900

    modify agent attributes

--- CovidAgents.py
+++ CovidAgents.py
@@ -1,40 +1,22 @@
-# from global_constants import TOTAL_AGENTS
+from global_constants import TOTAL_AGENTS
 import random
 
-n = 10  # n = 2380 # number of agents
-# agent_attributes = []
-vaccine_percentage = [50, 50]
-face_mask_comp = [50, 50]
-screening_comp = [50, 50]
-student_faculty_ratio = [1500/2380, 500/2380, 380/2380]
-# ---------------------------------------------------------------------
-vaccine_percentage2 = 0.5
-face_mask_comp2 = 0.5
-screening_comp2 = 0.5
-student_faculty_ratio2 = [1500 / 2380, 500 / 2380, 380 / 2380]
+# n = 10  # number of agents
+# n = 2380, on-campus: 1500, off-campus: 500, faculty: 380
+n = TOTAL_AGENTS
+TOTAL_AGENTS = 10
+
+vaccine_percentage = 0.5
+face_mask_comp = 0.5
+screening_comp = 0.5
+type_ratio = [0.2, 0.3]  # proportion of ["Off-campus Students", "Faculty"] - default value is "On-campus Student"
+subtype_ratio = [0.1, 0.2]  # proportion of ["Humanities", "Arts"] - default value is "STEM"
+initial_infection = 0.4  # proportion of students initially in the exposed state - should we make it number of students or a proportion?
+
+
 # ------------------------------------------------------------------------
-
 class agent:
-
     def initialize(self):
-        # global agents
-        agents = []
-        for i in range(n):
-
-            ag = agent()
-            ag.vaccinated = random.choices([0, 1], weights=vaccine_percentage)[0]  # vaccination status (0 = not vaccinated, 1 = vaccinated)
-            ag.face_mask = random.choices([0, 1], weights=face_mask_comp)[0]  # face_mask compliance
-            ag.screening = random.choices([0, 1], weights=screening_comp)[0] # screening test compliance
-            ag.type = random.choices(["On-campus Student", "Off-campus Student", "Faculty"], weights=student_faculty_ratio)[0]  # agent type - "Student" or "Faculty"
-            ag.subtype = random.choice(["STEM", "Humanities", "Arts"])  # agent subtype - should this be random? or are we also going to assign a specific percentage for this?
-            ag.seir = random.choice(["S", "E", "Ia", "Im", "Ie", "R"])  # agent (infection) states - maybe this shouldn't be random..?
-            ag.social = random.choice([0, 0.25, 0.75])  # reduction in socializing - not sure if I should make this {0, 1}
-            agents.append(ag)
-
-            print([ag.vaccinated, ag.face_mask, ag.screening, ag.type, ag.subtype, ag.seir, ag.social])
-
-
-    def initialize2(self):
         # global agents
         agents = []
         for i in range(n):
@@ -42,35 +24,67 @@ class agent:
             ag.vaccinated = 0  # vaccination status (0 = not vaccinated, 1 = vaccinated)
             ag.face_mask = 0  # face_mask compliance
             ag.screening = 0  # screening test compliance
-            ag.type = random.choices(["On-campus Student", "Off-campus Student", "Faculty"], weights=student_faculty_ratio)[0]  # agent type - "Student" or "Faculty"
-            ag.subtype = random.choice(["STEM", "Humanities", "Arts"])  # agent subtype - should this be random? or are we also going to assign a specific percentage for this?
-            ag.seir = random.choice(["S", "E", "Ia", "Im", "Ie", "R"])  # agent (infection) states - maybe this shouldn't be random..?
+            ag.type = "On-campus Student"
+            ag.subtype = "STEM"  # agent subtype/major (either STEM, Humanities, or Arts)
+            ag.seir = "S"  # agent infection states (either "S", "E", "Ia", "Im", "Ie", "R")
             ag.social = random.choice([0, 0.25, 0.75])  # reduction in socializing - not sure if I should make this {0, 1}
             agents.append(ag)
-            # print([ag.vaccinated, ag.face_mask, ag.screening, ag.type, ag.subtype, ag.seir, ag.social])
 
-        select_vaccine = random.choices(agents, k=int(n * vaccine_percentage2))
+
+        # VACCINATION: randomly select and assign vaccination to certain proportion of agents
+        select_vaccine = random.sample(agents, k=int(n * vaccine_percentage))
         for ag in select_vaccine:
             ag.vaccinated = 1
-            # print([ag.vaccinated, ag.face_mask, ag.screening, ag.type, ag.subtype, ag.seir, ag.social])
 
-
-        select_face_mask = random.choices(agents, k=int(n * face_mask_comp2))
+        # FACE MASK COMPLIANCE: randomly select and assign face mask compliance to certain proportion of agents
+        select_face_mask = random.sample(agents, k=int(n * face_mask_comp))
         for ag in select_face_mask:
             ag.face_mask = 1
 
-        select_screening = random.choices(agents, k=int(n * screening_comp2))
+        #  SCREENING TEST COMPLIANCE: randomly select and assign screening test compliance to certain proportion of agents
+        select_screening = random.sample(agents, k=int(n * screening_comp))
         for ag in select_screening:
             ag.screening = 1
 
-        print("-----------------------------------------------------------------------------")
+        # TYPE (ON-CAMPUS/OFF-CAMPUS/FACULTY): randomly select and assign a certain proportion of agents as "Off-campus Student" and "Faculty"
+        select_type = random.sample(agents, k=int(n * (type_ratio[0] + type_ratio[1])))
+        i = 0
+        while i < (len(select_type) * (type_ratio[0] / (type_ratio[0] + type_ratio[1]))):
+            select_type[i].type = "Off-campus Student"
+            i += 1
+        while i < len(select_type):
+            select_type[i].type = "Faculty"
+            i += 1
+
+        # SUBTYPE (STEM/HUMANITIES/ARTS): randomly select and assign a certain proportion of agents as "Humanities" and "Arts"
+        select_subtype = random.sample(agents, k=int(n * (subtype_ratio[0] + subtype_ratio[1])))
+        i = 0
+        while i < (len(select_subtype) * (subtype_ratio[0] / (subtype_ratio[0] + subtype_ratio[1]))):
+            select_subtype[i].subtype = "Humanities"
+            i += 1
+        while i < len(select_subtype):
+            select_subtype[i].subtype = "Arts"
+            i += 1
+
+        # INITIAL INFECTION: randomly select and assign agents that are initially infected
+        no_vaccine = []  # list of agents that haven't been vaccinated
+        for ag in agents:
+            if ag.vaccinated == 0:
+                no_vaccine.append(ag)
+        select_seir = random.sample(no_vaccine, k=int(n * initial_infection))  # randomly select initial number of agents (that haven't been vaccinated)
+        for ag in select_seir:
+                ag.seir = random.choice(["Ia", "Im", "Ie"])  # randomly assign one of the infected states to agents
+
+
+        # print all agents and their attributes
         for ag in agents:
             print([ag.vaccinated, ag.face_mask, ag.screening, ag.type, ag.subtype, ag.seir, ag.social])
 
 
 
 
-    # def infect(self):
+
+
 
 
 
