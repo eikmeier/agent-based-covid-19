38224919ec4eed348505ad565fed559ea0f175ea
Erik Nelson <erikdnel@gmail.com>
2021-07-08 14:39:04 -0500

    Add SIMULATION_LENGTH to global_constants

--- global_constants.py
+++ global_constants.py
@@ -1,5 +1,6 @@
 PASSING_TIME = 10  # Common passing time between classes, in minutes (302-307)
 TOTAL_AGENTS = 2380  # 2380 # 1500 on-campus students, 500 off-campus students, and 380 faculties (217-218)
+SIMULATION_LENGTH = 15 # Number of weeks the simulation should go for
 SPACE_CAPACITIES = {"Transit Space": 100 * TOTAL_AGENTS, "Dining Hall": 650, "Library": 300 * PASSING_TIME,
                     "Gym": 60 * PASSING_TIME}
 SPACE_RISK_MULTIPLIERS = {"Transit Space": 1, "Dining Hall": 1, "Library": 1, "Gym": 3,
